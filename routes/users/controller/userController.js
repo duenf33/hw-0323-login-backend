45cb86c198372a9adfac2927bd1bf937a3e40a64
fernando duenas <fernando39615@aim.com>
2021-03-29 09:44:24 -0400

    working on login

--- routes/users/controller/userController.js
+++ routes/users/controller/userController.js
@@ -23,14 +23,52 @@ module.exports = {
 	},
 	login: async (req, res) => {
 		try {
+			let foundUser = await User.findOne({ email: req.body.email });
+			if (!foundUser) {
+				res.json({ message: "email not found" });
+				throw { message: "email not found" };
+			}
+			let comparedPassword = await bcrypt.compare(
+				req.body.password,
+				foundUser.password
+			);
+			if (!comparedPassword) {
+				res.json({ message: "something went wrong, check email and password" });
+				throw { message: "something went wrong, check email and password" };
+			}
+			res.json({
+				message: "your logged in",
+				email: foundUser.email,
+			});
 		} catch (e) {
 			console.log(e);
 		}
 	},
 	updateUserPassword: async (req, res) => {
-		try {
+    try {
+      let foundUser = await User.findOne({ email: req.body.email })
+      if (!foundUser) {
+        res.json({message: "user not found"})
+        throw { message: "user not found" }
+      }
+      let comparedPassword = await bcrypt.compare(
+        req.body.oldPassword,
+        foundUser.password,
+      )
+      if (!comparedPassword) {
+        res.json({ message: "check email and password" })
+        throw {message: "check email and password"}
+      }
+      let salted = await bcrypt.genSalt(10)
+      let hashedNewPassword = await bcrypt.hash(req.body.newPassword, salted)
+      await User.findOneAndUpdate(
+        { email: req.body.email },
+        { password: hashedNewPassword },
+        {new: true},
+      )
 		} catch (e) {
 			console.log(e);
+			res.status(500).json(e);
 		}
 	},
 };
